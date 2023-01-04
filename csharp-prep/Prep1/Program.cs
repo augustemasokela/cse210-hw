ce5c578e13a610fca2b6c8924f62c46c39e959ca
Auguste Masokela <augusteipinge@gmail.com>
2023-01-04 19:38:31 +0200

    camel case

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        // change to camel case
+        
         Console.Write("What is your first name?  ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name?  ");
-        string lastname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine ($"Your name is {lastname}, {firstname} {lastname}");
+        Console.WriteLine ($"Your name is {lastName}, {firstName} {lastName}");
     }
 
     
