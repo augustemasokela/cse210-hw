42e93e6b9cd2525c88aaa83a47c09a7cb954fcc9
Auguste Masokela <augusteipinge@gmail.com>
2023-01-09 01:20:42 +0200

    pre3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,53 @@ using System;
 
 class Program
 {
+    public static int magicNumber =0;
+    public static Random rand = new Random();
+    public static bool GameOver =false;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        magicNumber=rand.Next(1,101);
+        int userGuess = 0;
+        Console.WriteLine(magicNumber);//computer pick for user
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuess=Convert.ToInt32(Console.ReadLine());
+            GuessNumber (userGuess);
+        }
+        while (GameOver == false);
+
+        Console.ReadLine();
+
+     }  
+     public static void GuessNumber (int userGuess)
+     {
+        // challange to stop and continue the game
+        int contPlay = 0;
+
+        if (userGuess < magicNumber)
+        Console.WriteLine("Lower");
+        else if (userGuess > magicNumber)
+        Console.WriteLine("Higher");
+        else
+        {
+            Console.WriteLine("you guess it. press 1 to continue or 2 to exit");
+            contPlay = Convert.ToInt32(Console.ReadLine());
+
+            while (contPlay != 1 && contPlay !=2 )
+            {
+                Console.Write("Invaid selection: press 1 or 2 ");
+                contPlay = Convert.ToInt32(Console.ReadLine());
+            }
+            if (contPlay.Equals(2))
+            GameOver= true;
+            else
+            magicNumber = rand.Next(1, 101);
+             
+        }
+     }
+
     }
-}
+    
